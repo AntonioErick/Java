319e3c40dafab0ac600b40f72d620b2cb0e48fa5
Antonio Erick <147534096+AntonioErick@users.noreply.github.com>
2025-01-09 14:04:18 -0300

    Add files via upload

---
+++ Projects/herança/src/Application/Program.java
@@ -0,0 +1,39 @@
+package Application;
+
+import Entities.Account;
+import Entities.BusinessAccount;
+import Entities.SavingsAccount;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Account acc = new Account(1001, "Alex", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+		
+		//UPCASTING é um casting da subclasse para a superclasse
+		
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		
+		//DOWNCASTING é um casting da superclasse para a subclasse
+		
+		BusinessAccount acc4 = (BusinessAccount)acc2;
+		acc4.loan(100.0);
+
+		//BusinessAccount acc5 = (BusinessAccount)acc3; como evitar esse tipo de erro:
+		if(acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		
+		if(acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount)acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
+	}
+
+}
